d579373dc2af699fd653e3b1ee05520b5c94c96c
Noah <47047865+NoahRJW@users.noreply.github.com>
2019-08-26 14:57:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import numpy as np
 import sklearn
 from sklearn import linear_model
 import pickle
+import time
+import constant
 import matplotlib.pyplot as pyplot
 from matplotlib.colors import from_levels_and_colors
+from mpl_toolkits.mplot3d import axes3d, Axes3D
 
 '''
 Created by Noah Wilson
@@ -15,7 +18,8 @@ How to use, you need to get student data in a perferably CSV format, which you c
 make sure you cover the data points the algorithm uses but you can also easily change these this code is free to use 
 for any, I would appreciate credit to parts of the code you usebut it is not required.
 '''
-studentdata = 'student-mat.csv'
+
+
 def graph():
     cmap, norm = from_levels_and_colors([0.0, 0.5, 1.5], ['red', 'blue'])
     fig = pyplot.figure()
@@ -34,8 +38,8 @@ def graph():
 def training():
     best_score = 0
     starttime = time.time()
-    while best_score < 0.95:
-        x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
+    while best_score < 0.90:
+        x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
         grph = linear_model.LinearRegression()
         grph.fit(x_train, y_train)
         accuracy = grph.score(x_test, y_test)
@@ -49,29 +53,32 @@ def training():
 
 # In sex / gender 0 = female : 1 = male
 
-data = pd.read_csv(studentdata, sep=";")
 
-print(data.head())
+data = pd.read_csv(constant.STUDENT_DATA_CSV, sep=";")
 
 data = data[["semester1", "semester2", "failures", "absences", "sex"]]
 predict = "semester2"
-X = np.array(data.drop([predict], 1))
-Y = np.array(data[predict])
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size = 0.1)
+x = np.array(data.drop([predict], 1))
+y = np.array(data[predict])
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-# Only train if you are running first or you want to get new data
-training()
 
 pickle_in = open('bestresults.pickle', 'rb')
 model = pickle.load(pickle_in)
 
-print('Co: \n', model.coef_)
-print('Inte: \n', +model.intercept_)
 
-predictions = model.predict(x_test).round()
+def printData():
+    print('Co: \n', model.coef_)
+    print('Inte: \n', +model.intercept_)
 
-for x in range(len(predictions)):
-    print(predictions[x], x_test[x], y_test[x])
+    predictions = model.predict(x_test).round()
+
+    for x in range(len(predictions)):
+        print(predictions[x], x_test[x], y_test[x])
 
 
-graph()
+if __name__ == '__main__':
+    training()
+    printData()
+    graph()
+
